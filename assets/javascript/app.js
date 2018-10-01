f5fbc15baf28aef483a1cd55b467da51dcf3b8fb
akatzer <akatzer@live.com>
2018-09-30 20:00:33 -0500

    added API calls for geolocation and weather and pushed that text to our weather information card

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     //hiding the <p> that i put the image url in to pull from later
     $("#url").hide();
- 
+
     //The array of background images, one item is in there as a default so there will always be a background picture available
     // var backgroundImage = ["https://i.ytimg.com/vi/xVYLqWUbyH8/maxresdefault.jpg"];
 
@@ -53,13 +53,13 @@ $(document).ready(function () {
         var file = e.target.files[0];
         //create a store ref
         var storageRef = firebase.storage().ref('images/' + file.name);
-        
+
         function getUrl() {
-        storageRef.getDownloadURL().then(function (url) {
-            $("#url").text(url);
-       
-        });
-    }
+            storageRef.getDownloadURL().then(function (url) {
+                $("#url").text(url);
+
+            });
+        }
 
         //upload file
         var task = storageRef.put(file);
@@ -68,18 +68,18 @@ $(document).ready(function () {
         task.on('state_changed',
             function progress(snapshot) {
                 var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                uploader.value = percentage;    
+                uploader.value = percentage;
             },
             function error(err) {
             },
-            function complete() { 
-                
+            function complete() {
+
                 getUrl();
             }
-            
+
         );
-        
-        
+
+
     });
 
     // click function to capture the user input
@@ -89,7 +89,6 @@ $(document).ready(function () {
         var email = $("#email").val().trim();
         var city = $("#city").val().trim();
         var state = $("#state :selected").text();
-        var description = $("#description").val().trim();
         var imageUrl = $("#url").text();
         var date = moment().format('MMMM Do YYYY');
 
@@ -99,13 +98,12 @@ $(document).ready(function () {
             email: email,
             city: city,
             state: state,
-            description: description,
             imageUrl: imageUrl,
             date: date,
         };
 
         //pushes new input into the database
-        if (name === "" && email === "" && city === "" && state === "" && description === ""()) {
+        if (name === "" && email === "" && city === "" && state === ""()) {
         }
         else {
             database.ref().push(newContent);
@@ -124,7 +122,6 @@ $(document).ready(function () {
         var email = childSnapshot.val().email;
         var city = childSnapshot.val().city;
         var state = childSnapshot.val().state;
-        var description = childSnapshot.val().description;
         var imageUrl = childSnapshot.val().imageUrl;
         var date = childSnapshot.val().date;
 
@@ -135,7 +132,7 @@ $(document).ready(function () {
             $("<p id='card-city'>").text("Photo Location : " + city + ", " + state),
             $("<p id='card-state'>").text("Contact me : " + email),
             $("<p id='card-state'>").text("Date Uploaded : " + date),
-            
+
         )
 
         //prepends the new card to the newCard div
@@ -153,7 +150,7 @@ $(document).ready(function () {
             // $("<p>").text("Contact me @ " + email),
             // $("<p>").text("Description: " + description),
             // )
-            
+
         })
 
         //randomizes our background image array to choose the next background image.
@@ -161,6 +158,36 @@ $(document).ready(function () {
         // $("body").css("background-image", "url('" + randomImage + "')");
 
     });
+
+    // api calls for geolocation and weather information
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position)
+        $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&sensor=false&key=AIzaSyBmULRulOLZfJ1ONuodqEQfe2RezgttsbY", function (data) {
+            var location = data.results[6].formatted_address;
+            var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+            // Here we are building the URL we need to query the database
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+                "q=" + location + "&units=imperial&appid=" + APIKey;
+            $("#gLoc").text(location)
+            // Here we run our AJAX call to the OpenWeatherMap API
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+
+                .then(function (response) {
+                    $("#oTemp").text("Temperature: " + Math.floor(response.main.temp) + " Degrees");
+                    $("#oWeather").text(response.weather[0].main);
+                    console.log(response)
+                })
+
+        })
+
+
+    });
+
+
 
 
 })
